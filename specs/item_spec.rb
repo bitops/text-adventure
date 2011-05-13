0ee2868b3f8ea252dbd1408f2585747d102e6533
Sebastian <invertedlambda@gmail.com>
2011-05-12 23:09:55 -0700

    get rid of those pesky specs - this is a playground!

--- specs/item_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__), "..", "item")
-
-describe Item do
-	it "should have a description" do
-		i = Item.new
-		i.description = "bla"
-		i.description.should == "bla"
-	end
-end
