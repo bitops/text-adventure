0ca44eb6118513f54fd61ad4705487f265624dd2
Sebastian Wittenkamp <a.sebastian.w@gmail.com>
2011-10-01 18:01:33 -0700

    First round of unit tests done for Room.

--- tests/room_test.rb
+++ tests/room_test.rb
@@ -1,16 +1,18 @@
 require 'minitest/autorun'
 require File.join(File.expand_path(File.dirname(__FILE__)), '..', 'lib', 'room')
 
-class TestUrlValidator < MiniTest::Unit::TestCase
+class RoomTest < MiniTest::Unit::TestCase
   
-	def test_room_has_properties
+  def setup
 		@room = Room.new
 		@room.description = "A room."
 		@room.rooms = []
 		@room.items = []
 		@room.info = "Info about the room."
-		@room.handle = "Test room"
-		
+		@room.handle = "Test room"    
+  end
+  
+	def test_room_has_properties
 		assert_equal("A room.", @room.description)
 		assert_equal([], @room.rooms)
 		assert_equal([], @room.items)
@@ -18,10 +20,30 @@ class TestUrlValidator < MiniTest::Unit::TestCase
 		assert_equal("Test room", @room.handle)
 	end
 	
-	def test_rooms_knows_adjacent_rooms
-	  raise "not yet"
+	def test_room_knows_if_it_is_starting_location
+	 @room.starting_location = false
+	 refute(@room.starting_location?)
+	 @room.starting_location = true
+	 assert(@room.starting_location?)
 	end
 	
-	# TODO: starting location
+	def test_rooms_knows_adjacent_rooms
+	  @room.rooms = {
+	    "north" => "room_a",
+	    "south" => "room_b"
+	  }
+	  assert(@room.has_room_to_the?("north"))
+	  assert(@room.has_room_to_the?("south"))
+	  refute(@room.has_room_to_the?("west"))
+	  refute(@room.has_room_to_the?("east"))
+	end
 	
 end
+
+
+
+
+
+
+
+
