72add127010f6c17d99ab314750f17cffadcffc3
Sebastian Wittenkamp <a.sebastian.w@gmail.com>
2011-10-01 18:57:17 -0700

    Cleaned up the engine a little bit. There's a dependency graph here that should be cleaned up. I think it'd be a little cleaner if the engine started up and was just injected with the data it needs to start. There's a little too much (okay, way too much) intelligence in the Engine constructor.

--- bin/engine.rb
+++ bin/engine.rb
@@ -14,15 +14,27 @@ ARGV.clear
 
 class Engine
   def initialize(location_data_file, message_data_file)
+    # TODO: this should move out of the engine. data should be passed in pre-loaded.
     @loader = GameDataLoader.new
+    # TODO: this should be injected into the engine also.
     @ctl = InputController.new
-    locations = @loader.load_location_data(location_data_file)
-    @messages = @loader.load_message_data(message_data_file)
-    @splash_message = @messages["splash"]
+    locations = load_locations(location_data_file)
+    messages = load_messages(message_data_file)
+    @splash_message = messages["splash"]
     starting_location = locations.find {|location| location.starting_location?}
-    # Initializing controller
-    avatar = Avatar.new(starting_location)
-    @ctl.messages = @messages
+    initialize_controller messages, Avatar.new(starting_location)
+  end
+  
+  def load_locations(location_data_file)
+    @loader.load_location_data(location_data_file)
+  end
+  
+  def load_messages(message_data_file)
+    @loader.load_message_data(message_data_file)
+  end
+  
+  def initialize_controller(messages, avatar)
+    @ctl.messages = messages
     @ctl.avatar = avatar
     @ctl.initialize_message
   end
