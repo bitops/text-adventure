bb14a9779ff9ca0ab890755e39f4f8882b0bc50e
Sebastian Wittenkamp <a.sebastian.w@gmail.com>
2011-10-01 18:06:04 -0700

    renamed location reader to current_room internally

--- lib/avatar.rb
+++ lib/avatar.rb
@@ -1,18 +1,21 @@
 class Avatar	
-	attr_reader :location
+	
+	def location
+	  @current_room
+	end
 
 	def initialize(starting_location)
-		@location = starting_location
+		@current_room = starting_location
 	end
 
 	def can_move?(direction)
-		@location.has_room_to_the?(direction)
+		@current_room.has_room_to_the?(direction)
 	end
 
 	def move(direction)
 		if can_move?(direction)
-			new_location = @location.rooms[direction]
-			@location = new_location
+			new_room = @current_room.rooms[direction]
+			@current_room = new_room
 			true
 		else
 			false
