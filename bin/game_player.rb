7900b434d18f56e27640ac56c097340025ad77d0
Sebastian <a.sebastian.w@gmail.com>
2011-10-06 20:08:13 -0700

    remove unnecessary clear

--- bin/game_player.rb
+++ bin/game_player.rb
@@ -3,10 +3,9 @@ GAME_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 require File.join(GAME_ROOT, 'lib', 'bootstrap')
 require File.join(GAME_ROOT, 'lib', 'game')
 
+# TODO: this could be a little nicer.
 location_data_file = File.absolute_path(File.join(GAME_ROOT, "#{ARGV[0]}"))
 message_data_file = File.absolute_path(File.join(GAME_ROOT, "#{ARGV[1]}"))
-
-ARGV.clear
 
 # main
 bootstrap = Bootstrap.new(location_data_file, message_data_file)
