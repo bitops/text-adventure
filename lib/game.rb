91e50b6250446ef88813a81ce6c9f5ece0b967f6
Sebastian <a.sebastian.w@gmail.com>
2011-10-06 20:19:33 -0700

    prepare for test

--- lib/game.rb
+++ lib/game.rb
@@ -2,11 +2,15 @@ require File.join(File.expand_path(File.dirname(__FILE__)), 'engine')
 
 class Game
   def initialize(bootstrap)
-    @engine = Engine.new(bootstrap.controller)
+    @engine = engine.new(bootstrap.controller)
     @engine.splash_message = bootstrap.splash_message
   end
   
   def play
     @engine.start
+  end
+  
+  def engine
+    Engine
   end
 end
