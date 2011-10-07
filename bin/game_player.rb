1be5583f0a9bec9b329e05cf91456b5233133714
Sebastian <a.sebastian.w@gmail.com>
2011-10-06 20:10:06 -0700

    tighten it up a little more

--- bin/game_player.rb
+++ bin/game_player.rb
@@ -3,9 +3,13 @@ GAME_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 require File.join(GAME_ROOT, 'lib', 'bootstrap')
 require File.join(GAME_ROOT, 'lib', 'game')
 
+def lookup_file_from(path)
+  File.absolute_path(File.join(GAME_ROOT, path))
+end
+
 # TODO: this could be a little nicer.
-location_data_file = File.absolute_path(File.join(GAME_ROOT, "#{ARGV[0]}"))
-message_data_file = File.absolute_path(File.join(GAME_ROOT, "#{ARGV[1]}"))
+location_data_file = lookup_file_from "#{ARGV[0]}"
+message_data_file = lookup_file_from "#{ARGV[1]}"
 
 # main
 bootstrap = Bootstrap.new(location_data_file, message_data_file)
